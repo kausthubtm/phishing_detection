90e4f38016d8e8d198f32222eaaaa5a7e95ce4c9
Kausthub Thekke Madathil <kausthubtm6671@gmail.com>
2021-02-27 18:43:25 +0530

    model and basic front-end done

--- hackverse/Phishing_detector/XGBoost.py
+++ hackverse/Phishing_detector/XGBoost.py
@@ -22,6 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 xgb = XGBClassifier(learning_rate=0.4,max_depth=7,use_label_encoder=False)
 xgb.fit(X_train, y_train)
 y_test_xgb = xgb.predict(X_test)
+print(X_test)
 y_train_xgb = xgb.predict(X_train)
 acc_train_xgb = accuracy_score(y_train,y_train_xgb)
 acc_test_xgb = accuracy_score(y_test,y_test_xgb)
@@ -29,5 +30,4 @@ print("XGBoost: Accuracy on training Data: {:.3f}".format(acc_train_xgb))
 print("XGBoost : Accuracy on test Data: {:.3f}".format(acc_test_xgb))
 
 # Saving the model  
-pickle.dump(xgb, open("XGBoostClassifier.pickle.dat", "wb"))
-loaded_model = pickle.load(open("XGBoostClassifier.pickle.dat", "rb"))
+pickle.dump(xgb, open('trained_model.model', 'wb'))
--- hackverse/Phishing_detector/views.py
+++ hackverse/Phishing_detector/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .feature_extraction import featureExtraction
+from .predict import predict
 
 # Create your views here.
 
 def get_index(request):
+    features = []
+    features = featureExtraction('sieck-kuehlsysteme.de')
+    predict(features)
+
     return render(request, 'index.html')
